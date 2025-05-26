ba5b33bfe4acaea47840812af0b9e935b82e43c1
Tomasz Pluskiewicz <tpluscode@users.noreply.github.com>
2025-05-26 13:56:30 +0200

    fix: broken Array#filter usage

--- index.ts
+++ index.ts
@@ -1,23 +1,23 @@
-import { AnyContext, AnyPointer, GraphPointer } from 'clownface'
+import type { AnyContext, AnyPointer, GraphPointer } from 'clownface'
 import type { BlankNode, NamedNode, Literal, DatasetCore } from '@rdfjs/types'
 
-export function isGraphPointer(arg: AnyPointer | unknown): arg is GraphPointer {
+export function isGraphPointer(arg: AnyPointer | null | undefined | unknown): arg is GraphPointer {
   return typeof arg === 'object' && !!arg && 'term' in arg
 }
 
-export function isResource<D extends DatasetCore>(arg: AnyPointer<AnyContext, D> | unknown): arg is GraphPointer<BlankNode | NamedNode, D> {
+export function isResource<D extends DatasetCore>(arg: AnyPointer<AnyContext, D> | null | undefined): arg is GraphPointer<BlankNode | NamedNode, D> {
   return isGraphPointer(arg) && (arg.term.termType === 'BlankNode' || arg.term.termType === 'NamedNode')
 }
 
-export function isNamedNode<D extends DatasetCore>(arg: AnyPointer<AnyContext, D> | unknown): arg is GraphPointer<NamedNode, D> {
+export function isNamedNode<D extends DatasetCore>(arg: AnyPointer<AnyContext, D> | null | undefined): arg is GraphPointer<NamedNode, D> {
   return isGraphPointer(arg) && arg.term.termType === 'NamedNode'
 }
 
-export function isBlankNode<D extends DatasetCore>(arg: AnyPointer<AnyContext, D> | unknown): arg is GraphPointer<BlankNode, D> {
+export function isBlankNode<D extends DatasetCore>(arg: AnyPointer<AnyContext, D> | null | undefined): arg is GraphPointer<BlankNode, D> {
   return isGraphPointer(arg) && arg.term.termType === 'BlankNode'
 }
 
-export function isLiteral<D extends DatasetCore>(arg: AnyPointer<AnyContext, D> | unknown, dtOrLang?: NamedNode | string | number): arg is GraphPointer<Literal, D> {
+export function isLiteral<D extends DatasetCore>(arg: AnyPointer<AnyContext, D> | undefined, dtOrLang?: NamedNode | string | number): arg is GraphPointer<Literal, D> {
   const itIsLiteral = isGraphPointer(arg) && arg.term.termType === 'Literal'
   if (itIsLiteral && typeof dtOrLang === 'string') {
     return arg.term.language === dtOrLang
